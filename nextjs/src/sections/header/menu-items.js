bbe98db277ea8804820d1cc26b70492bfbd83147
Arvind Yadav <arvindyadav@gakudoai.onmicrosoft.com>
2024-12-14 06:59:16 +0000

    gai & MS logo

--- nextjs/src/sections/header/menu-items.js
+++ nextjs/src/sections/header/menu-items.js
@@ -12,6 +12,20 @@ const iconStyle = {height: 16, width: 16, mb: "-3px", mr: 0.5};
 
 export const menuItems = [
     {
+        icon: <DashboardIcon sx={iconStyle}/>,
+        label: "Dashboard",
+        tooltip: "Dashboard Page",
+        path: "/dashboard",
+        authenticated: true
+    },
+    {
+    icon: <ChatGptIcon width={20} height={20} sx={iconStyle} />, // Using ChatGPT icon directly
+    label: "Ask Anything",
+    tooltip: "AI Guide", // Updated tooltip to match functionality
+    path: "/extensions/chatgpt", // Direct path to ChatGPT
+    authenticated: true
+},
+    {
         icon: <BusinessIcon sx={iconStyle}/>,
         label: "About Us",
         tooltip: "README.md Documentations",
@@ -25,46 +39,5 @@ export const menuItems = [
         path: "/login",
         authenticated: false
     },
-    {
-        icon: <DashboardIcon sx={iconStyle}/>,
-        label: "Dashboard",
-        tooltip: "Dashboard Page",
-        path: "/dashboard",
-        authenticated: true
-    },
-//    {
-//        icon: <ChatGptIcon sx={iconStyle}/>,
-//        label: "Ask Anything",
-//        tooltip: "Feature List",
-//        path: "/chatgpt",
-//        authenticated: true
-//    },
-//    {
-//        icon: <PeopleAltIcon sx={iconStyle}/>,
-//        label: "Users",
-//        tooltip: "Users",
-//        path: "/users",
-//        authenticated: true
-//    },
-//    {
-//        icon: <ExtensionIcon sx={iconStyle}/>,
-//        label: "Ask Anything",
-//        tooltip: "Feature List",
-//        authenticated: true,
-//        subItems: [
-//            {
-//                icon: <ChatGptIcon width={20} height={20}/>,
-//                label: "AI Guide",
-//                path: "/extensions/chatgpt",
-//                authenticated: true
-//            }
-//        ]
-//    },
-    {
-    icon: <ChatGptIcon width={20} height={20} sx={iconStyle} />, // Using ChatGPT icon directly
-    label: "Ask Anything",
-    tooltip: "AI Guide", // Updated tooltip to match functionality
-    path: "/extensions/chatgpt", // Direct path to ChatGPT
-    authenticated: true
-},
+    
 ];
